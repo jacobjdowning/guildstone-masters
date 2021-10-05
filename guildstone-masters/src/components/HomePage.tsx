1fba33e2ddd575048bdea87a58cc76e8b7689a2a
Jacob Downing <jacobjdowning@gmail.com>
2021-10-05 14:34:09 -0400

    implements homepage navigation from user input of guild info

--- guildstone-masters/src/components/HomePage.tsx
+++ guildstone-masters/src/components/HomePage.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FunctionComponent } from 'react';
-import Card from './CardView';
+import { useHistory } from 'react-router';
 
+import Card from './CardView';
+import { slugify } from '../logic/stringUtils';
 
 const HomePage:FunctionComponent = () => {
+    const history = useHistory();
+
+    const [region, setRegion] = useState<string>('US');
+    const [realmName, setRealmName] = useState<string>('');
+    const [guildName, setGuildName] = useState<string>('');
+
+    function changeRegion(event:React.ChangeEvent<HTMLSelectElement>){
+        setRegion(event.target.value)
+    }
+    function changeRealmName(event:React.ChangeEvent<HTMLInputElement>){
+        //change to slug
+        setRealmName(slugify(event.target.value))
+    }
+    function changeGuildName(event:React.ChangeEvent<HTMLInputElement>){
+        //change to slug
+        setGuildName(slugify(event.target.value))
+    }
+    function confirmGuild(event:React.MouseEvent<HTMLInputElement>){
+        history.push(`/${region}/${realmName}/${guildName}`)
+        event.preventDefault()
+    }
+
     return <React.Fragment>
         <h1> Guildstone Masters </h1>
         <Card>
             <form>
-                <select>
+                <select value={region} onChange={changeRegion}>
                     <option value='us'>US</option>
                     <option value='eu'>EU</option>
                     <option value='cn'>CN</option>
                     <option value='kr'>KR</option>
                     <option value='tw'>TW</option>
                 </select>
-                <label>Realm:</label>
-                <input type="text"></input>
-                <label>Guild</label>
-                <input type="text"></input>
+                <label htmlFor="realm-name">Realm:</label>
+                <input type="text" id="realm-name" value={realmName} onChange={changeRealmName}></input>
+                <label htmlFor="guild-name">Guild:</label>
+                <input type="text" id="guild-name" value={guildName} onChange={changeGuildName}></input>
+                <input type="submit" value="Go" onClick={confirmGuild}/>
             </form>
         </Card>
     </React.Fragment>
---
+++ guildstone-masters/src/tests/HomePage.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import HomePage from '../components/HomePage';
+import { useHistory } from 'react-router-dom';
+
+jest.mock('react-router', () => {
+    const originalModule = jest.requireActual('react-router')
+    const push = jest.fn()
+    return {
+        __esModule:true,
+        ...originalModule,
+        useHistory: () => {
+            return { push: push }
+        }
+    }
+})
+
+describe('Tests around the homepage of the app, where users select their guild', () => {
+    test('Homepage has all its components to select a guild', ()=> {
+        const wrapper = shallow(<HomePage />)
+        expect(wrapper.containsAllMatchingElements([
+            <label htmlFor="realm-name">Realm:</label>,
+            <label htmlFor="guild-name">Guild:</label>,
+            <input type="text" id="realm-name"></input>,
+            <input type="text" id="guild-name"></input>,
+            <option value='us'>US</option>,
+            <option value='eu'>EU</option>,
+            <option value='kr'>KR</option>,
+            <input type="submit" value="Go"/>
+        ])).toEqual(true)
+
+    })
+
+    test('When a region, realm, and guild name are entered and the submit button' +
+    ' is clicked, the user should be sent to the correct GuildProfile with the correct ' +
+    ' props - tested with mocked \'useHistory\' hook', ()=>{
+        
+        const wrapper = shallow(<HomePage />);
+        wrapper.find('select').simulate('change', {target:{value: 'us'}})
+        wrapper.find('#realm-name').simulate('change', {target:{value:'Icecrown'}})
+        wrapper.find('#guild-name').simulate('change', {target:{value:'French Toast'}})
+        wrapper.find('input[type=\"submit\"]').simulate('click', {preventDefault: jest.fn()})
+
+        expect(useHistory().push).toBeCalledWith('/us/icecrown/french-toast') 
+    })
+})
